073e70d67e327de38c0c9db16c18fa83c62fd934
Sebastiaan Mathot <s.mathot@cogsci.nl>
2019-07-24 18:25:57 +0200

    JupyterNotebook: remember last folder and catch errors when reading notebook

--- opensesame_extensions/JupyterNotebook/JupyterNotebook.py
+++ opensesame_extensions/JupyterNotebook/JupyterNotebook.py
@@ -18,10 +18,12 @@ along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from libopensesame.py3compat import *
+import os
 import re
 import nbformat
 from qtpy.QtWidgets import QFileDialog
 from libqtopensesame.extensions import BaseExtension
+from libqtopensesame.misc.config import cfg
 from libqtopensesame.misc.translate import translation_context
 _ = translation_context(u'JupyterNotebook', category=u'extension')
 
@@ -50,13 +52,17 @@ class JupyterNotebook(BaseExtension):
         path = QFileDialog.getOpenFileName(
             self.main_window,
             _(u'Open Jupyter/ IPython Notebook'),
-            filter=u'Notebooks (*.ipynb)'
+            filter=u'Notebooks (*.ipynb)',
+            directory=cfg.file_dialog_path
         )
         if isinstance(path, tuple):
             path = path[0]
         if not path:
             return
+        cfg.file_dialog_path = os.path.dirname(path)
         code = self._notebook_to_code(path)
+        if not code:
+            return
         self.extension_manager.fire(u'ide_new_file', source=code)
 
     def _export_ipynb(self):
@@ -64,18 +70,28 @@ class JupyterNotebook(BaseExtension):
         path = QFileDialog.getSaveFileName(
             self.main_window,
             _(u'Open Jupyter/ IPython Notebook'),
-            filter=u'Notebooks (*.ipynb)'
+            filter=u'Notebooks (*.ipynb)',
+            directory=cfg.file_dialog_path
         )
         if isinstance(path, tuple):
             path = path[0]
         if not path:
             return
+        cfg.file_dialog_path = os.path.dirname(path)
         code = self.extension_manager.provide(u'ide_current_source')
         self._code_to_notebook(code, path)
 
     def _notebook_to_code(self, path):
 
-        nb = nbformat.read(path, as_version=4)
+        try:
+            nb = nbformat.read(path, as_version=4)
+        except Exception as e:
+            self.extension_manager.fire(
+                u'notify',
+                message=_(u'Failed to read notebook. See console for details.')
+            )
+            self.console.write(e)
+            return
         py_cells = []
         for cell in nb['cells']:
             if cell['cell_type'] == 'markdown':
