32645756a80f32d78f23a9ef8422e4cf9044654b
Sebastiaan Mathot <s.mathot@cogsci.nl>
2019-09-05 17:28:47 +0200

    Bump to 0.1.9

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ EXTENSIONS = [
     u'FindInFiles',
     u'JupyterConsole',
     u'JupyterNotebook',
-    u'QuickSelector'
+    u'QuickSelector',
+    u'WorkspaceExplorer'
 ]
 
 
@@ -106,7 +107,7 @@ def data_files():
 
 setup(
     name='rapunzel',
-    version='0.1.7',
+    version='0.1.9',
     description='Turns OpenSesame into a Python code editor',
     author='Sebastiaan Mathot',
     author_email='s.mathot@cogsci.nl',
