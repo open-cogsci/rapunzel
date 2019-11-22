7e6dbe03301b1f4e865ebeee71c08a873e5ad68c
Sebastiaan Mathot <s.mathot@cogsci.nl>
2019-11-22 11:08:58 +0100

    RapunzelWelcome: fix bug when file is opened before startup event

--- opensesame_extensions/RapunzelWelcome/RapunzelWelcome.py
+++ opensesame_extensions/RapunzelWelcome/RapunzelWelcome.py
@@ -32,6 +32,9 @@ class RapunzelWelcome(BaseExtension):
     @BaseExtension.as_thread(wait=500)
     def event_startup(self):
 
+        if self.extension_manager['OpenSesameIDE']._current_editor():
+            # Only show the startup tab if no editors are open
+            return
         with safe_open(self.ext_resource(u'rapunzel_welcome.md')) as fd:
             md = fd.read()
         self.tabwidget.open_markdown(
@@ -51,7 +54,7 @@ class RapunzelWelcome(BaseExtension):
 
     def event_register_editor(self, editor):
 
-        if not self._widget:
+        if not hasattr(self, '_widget') or not self._widget:
             return
         index = self.tabwidget.indexOf(self._widget)
         self._widget = None
