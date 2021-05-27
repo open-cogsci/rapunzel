42b714d69bcf4b9943969fd75e928162e565d01a
smathot <s.mathot@cogsci.nl>
2021-05-27 14:12:06 +0200

    DataViewer: image previews are resizable (closes #38)

--- opensesame_extensions/DataViewer/data_viewer_inspectors/inspect_PngImageFile.py
+++ opensesame_extensions/DataViewer/data_viewer_inspectors/inspect_PngImageFile.py
@@ -18,13 +18,42 @@ along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from libopensesame.py3compat import *
+import math
 from PIL.ImageQt import ImageQt
 from qtpy.QtGui import QPixmap
+from qtpy.QtCore import QSize
 from qtpy.QtWidgets import QLabel
+
+
+class ImageLabel(QLabel):
+    """Implements a label with a resizable image that maintains its original
+    aspect ratio.
+    """
+    
+    def __init__(self, img):
+        
+        QLabel.__init__(self)
+        self._ratio = img.size[0] / img.size[1]
+        self._pixmap = QPixmap.fromImage(ImageQt(img))
+        self.setMinimumSize(QSize(10, 10))
+        self.setScaledContents(True)
+        self.setPixmap(self._pixmap)
+        
+    def resizeEvent(self, e):
+
+        size = e.size()
+        height = size.height()
+        width = size.width()
+        ratio = width / height
+        if math.isclose(ratio, self._ratio):
+            return
+        if ratio > self._ratio:
+            new_size = QSize(height * self._ratio, height)
+        elif ratio < self._ratio:
+            new_size = QSize(width, width / self._ratio)
+        self.resize(new_size)
 
 
 def inspect_PngImageFile(value):
 
-    label = QLabel()
-    label.setPixmap(QPixmap.fromImage(ImageQt(value)))
-    return label
+    return ImageLabel(value)
