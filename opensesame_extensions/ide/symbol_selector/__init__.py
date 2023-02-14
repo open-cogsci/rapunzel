bdda0de093743f7186134e97e1a24f8f81f6ff3a
smathot <s.mathot@cogsci.nl>
2023-02-14 21:29:57 +0100

    symbol_selector: add missing label

--- opensesame_extensions/ide/symbol_selector/__init__.py
+++ opensesame_extensions/ide/symbol_selector/__init__.py
@@ -1,6 +1,7 @@
 """Quickly jump to functions, classes, and other symbol definitions"""
 
 tooltip = "Quickly jump to functions, classes, and other symbol definitions"
+label = "Symbol selector"
 shortcut = "Ctrl+R"
 icon = "edit-find"
 modes = ["ide"]
