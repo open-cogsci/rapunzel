2b2dfba7a03eae3f111bbfa5f87efc9398227e9f
smathot <s.mathot@cogsci.nl>
2023-07-03 16:41:20 +0200

    Update doc files

--- doc/publish.sh
+++ doc/publish.sh
@@ -4,6 +4,7 @@ rm -Rf output/*
 mkdir output
 cp -R static/* output
 python build-menu.py --publish
+cp -R static/* output
 pelican -s publishconf.py
 cp output/index/index.html output/index.html
 python parse-theme.py
